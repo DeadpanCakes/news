1f9863c245e4edf0c5310c7d4cfeb171a353ce96
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-10-25 17:31:16 -0700

    Added markup for Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,52 @@
+const Footer = () => {
+  return (
+    <footer>
+      <form>
+        <h3>Sign up to our newsletters</h3>
+        <p>Enter your email address to get started</p>
+        <input placeholder="Your email" />
+        <button>SIGN UP</button>
+      </form>
+      <nav>
+        <ul>
+          <li>Contact us</li>
+          <li>Help</li>
+          <li>About us</li>
+          <li>Privacy policy</li>
+          <li>Cookie policy</li>
+          <li>Terms & conditions</li>
+        </ul>
+      </nav>
+      <br />
+      <nav>
+        <ul>
+          <li>Advertise</li>
+          <li>Write for us</li>
+          <li>Events</li>
+          <li>Science jobs</li>
+          <li>Syndication</li>
+          <li>RSS feeds</li>
+          <li>Gift subscriptions</li>
+          <li>Student subscriptions</li>
+          <li>Educational subscriptions</li>
+          <li>Corporate subscriptions</li>
+        </ul>
+      </nav>
+      <button>COOKIE SETTINGS</button>
+      <div>
+        <h3>GET THE APP</h3>
+      </div>
+      <div>
+        <h3>FOLLOW US</h3>
+        <ul>
+            <li>Facebook</li>
+            <li>Twitter</li>
+            <li>Linkedin</li>
+            <li>Pintrest</li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
