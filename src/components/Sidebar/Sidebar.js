a56333b7f146b958fdc5b8fee2de2e5d7f493267
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-10-26 15:12:25 -0700

    added layout styling to sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,8 +1,10 @@
+import "./Sidebar.css";
+
 const Sidebar = () => {
   return (
-    <aside>
+    <aside className="sideBar">
       <header>
-        <ul>
+        <ul className="sideBarCat">
           <li>
             <h3>Trending</h3>
           </li>
@@ -18,29 +20,34 @@ const Sidebar = () => {
         </ul>
       </header>
       <ul>
-        <li>
+        <li className="topArticle">
           <h2>
             How much less likely are you to spread covid-19 if you're
             vaccinated?
           </h2>
+          <h1 className="articleRank">1</h1>
         </li>
-        <li>
+        <li className="topArticle">
           <h2>Humans are hardwired to feel others' pain</h2>
+          <h1 className="articleRank">2</h1>
         </li>
-        <li>
+        <li className="topArticle">
           <h2>
             Sperm quality has been declining for 16 years amovg men in the US
           </h2>
+          <h1 className="articleRank">3</h1>
         </li>
-        <li>
+        <li className="topArticle">
           <h2>
             AI can turn a collection of 2D images into an explorable 3D world
           </h2>
+          <h1 className="articleRank">4</h1>
         </li>
-        <li>
+        <li className="topArticle">
           <h2>
             Glass flask catalysed famous Miller-Urey origin-of-life experiment
           </h2>
+          <h1 className="articleRank">5</h1>
         </li>
       </ul>
     </aside>
