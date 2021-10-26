27aa3bc2ea0780149b17e75533f1d77d0d570842
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-10-25 17:08:06 -0700

    added skeleton markup for Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,55 @@
+const Header = () => {
+  return (
+    <header>
+      <hgroup>
+        <h3>NEWSETTERS</h3>
+        <h4>Sign up to read our regular email newsletters</h4>
+      </hgroup>
+      <a href="https://www.newscientist.com/">
+        <img alt="The NewScientist logo"></img>
+      </a>
+      <button>SUBSCRIBE AND SAVE 69%</button>
+      <nav>
+        <ul>
+          <li>
+            <a href="https://www.newscientist.com/section/news/">News</a>
+          </li>
+          <li>
+            <a href="https://www.newscientist.com/podcasts/">Podcasts</a>
+          </li>
+          <li>
+            <a href="https://www.newscientist.com/video/">Video</a>
+          </li>
+          <li>
+            <a href="https://www.newscientist.com/subject/technology/">
+              Technology
+            </a>
+          </li>
+          <li>
+            <a href="https://www.newscientist.com/subject/space/">Space</a>
+          </li>
+          <li>
+            <a href="https://www.newscientist.com/subject/physics/">Physics</a>
+          </li>
+          <li>
+            <a href="https://www.newscientist.com/subject/health/">Health</a>
+          </li>
+          <li>
+            <a href="https://www.newscientist.com/article/2286218-ancient-lake-in-marss-gale-crater-may-have-actually-been-a-small-pond/#">
+              More
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <nav>
+        <ul></ul>
+      </nav>
+      <a href="https://www.newscientist.com/login/">
+        <button>Sign In</button>
+      </a>
+      <button>Search</button>
+    </header>
+  );
+};
+
+export default Header;
