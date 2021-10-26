48bc586a56c1ab92ad103366edc0ab42a8383caa
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-10-25 17:18:01 -0700

    Made skeleton of markup for Sidebar

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,50 @@
+const Sidebar = () => {
+  return (
+    <aside>
+      <header>
+        <ul>
+          <li>
+            <h3>Trending</h3>
+          </li>
+          <li>
+            <h3>Latest</h3>
+          </li>
+          <li>
+            <h3>Video</h3>
+          </li>
+          <li>
+            <h3>Free</h3>
+          </li>
+        </ul>
+      </header>
+      <ul>
+        <li>
+          <h2>
+            How much less likely are you to spread covid-19 if you're
+            vaccinated?
+          </h2>
+        </li>
+        <li>
+          <h2>Humans are hardwired to feel others' pain</h2>
+        </li>
+        <li>
+          <h2>
+            Sperm quality has been declining for 16 years amovg men in the US
+          </h2>
+        </li>
+        <li>
+          <h2>
+            AI can turn a collection of 2D images into an explorable 3D world
+          </h2>
+        </li>
+        <li>
+          <h2>
+            Glass flask catalysed famous Miller-Urey origin-of-life experiment
+          </h2>
+        </li>
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
