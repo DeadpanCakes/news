a61055a05ddc4d90ccfeacedb96fab67d44ef978
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-10-26 13:39:18 -0700

    added components to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import Header from "./components/Header/Header";
+import Article from "./components/Article/Article";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Recommended from "./components/Recommended/Recommended";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Article />
+      <Sidebar />
+      <Recommended />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
