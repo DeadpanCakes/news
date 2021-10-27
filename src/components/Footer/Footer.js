6039c21755993a3ebcae9dfeddc94e61a8cf8cab
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-10-26 17:40:44 -0700

    styled footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,10 +9,16 @@ const Footer = () => {
           Enter your email address to get started
         </label>
         <input id="newsletterField" placeholder="Your email" />
-        <button onClick={(e) => {e.preventDefault()}}>SIGN UP</button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+          }}
+        >
+          SIGN UP
+        </button>
       </form>
       <nav>
-        <ul>
+        <ul className="siteInfoLinks">
           <li>Contact us</li>
           <li>Help</li>
           <li>About us</li>
@@ -21,14 +27,16 @@ const Footer = () => {
           <li>Terms & conditions</li>
         </ul>
       </nav>
-      <br />
-      <nav>
-        <ul>
+      <hr />
+      <nav className="footerNav">
+        <ul className="siteInfoLinks">
           <li>Advertise</li>
           <li>Write for us</li>
           <li>Events</li>
           <li>Science jobs</li>
           <li>Syndication</li>
+        </ul>
+        <ul className="siteInfoLinks">
           <li>RSS feeds</li>
           <li>Gift subscriptions</li>
           <li>Student subscriptions</li>
@@ -36,18 +44,20 @@ const Footer = () => {
           <li>Corporate subscriptions</li>
         </ul>
       </nav>
-      <button>COOKIE SETTINGS</button>
-      <div>
-        <h3>GET THE APP</h3>
-      </div>
-      <div>
-        <h3>FOLLOW US</h3>
-        <ul className="socialNav">
-          <li>Facebook</li>
-          <li>Twitter</li>
-          <li>Linkedin</li>
-          <li>Pintrest</li>
-        </ul>
+      <button className="cookiesBtn">COOKIE SETTINGS</button>
+      <div className="subContainer">
+        <div>
+          <h3>GET THE APP</h3>
+        </div>
+        <div className="footerSocialNav">
+          <h3 className="socialNavHeader">FOLLOW US</h3>
+          <ul className="socialNav">
+            <li>Facebook</li>
+            <li>Twitter</li>
+            <li>Linkedin</li>
+            <li>Pintrest</li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
